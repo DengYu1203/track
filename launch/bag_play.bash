a7a5c63216164019b9379dc1d87213ff796bd416
DengYu1203 <sg110333@gmail.com>
2020-12-14 17:16:56 +0800

    add bag player bash

--- launch/bag_play.bash
+++ launch/bag_play.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
-for f in /data2/nuScenes/bag_sweep_final/log6*.bag
+rate=0.5
+bag_path_1=/data2/nuScenes/bag_sweep_final/log46_1537295833898809_scene-0626.bag
+bag_path_2=/data2/nuScenes/bag_sweep_final/log3*.bag
+for f in $bag_path_1
 do
-  rosbag play $f --clock
+  rosbag play $f --clock -r $rate
 done
