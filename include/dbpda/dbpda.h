816d0b602be4b2506815c5f3e89189d45a46a96c
DengYu1203 <sg110333@gmail.com>
2021-01-19 16:33:20 +0800

    add kf and overlay frames num

--- include/dbpda/dbpda.h
+++ include/dbpda/dbpda.h
@@ -17,9 +17,10 @@
 // opencv for tracking
 #include <cv_bridge/cv_bridge.h>
 #include <opencv-3.3.1-dev/opencv2/core.hpp>
+#include <opencv-3.3.1-dev/opencv2/core/eigen.hpp>
 #include <opencv-3.3.1-dev/opencv2/highgui.hpp>
-#include <opencv-3.3.1-dev/opencv2/imgproc.hpp>
-#include <opencv-3.3.1-dev/opencv2/calib3d.hpp>
+// #include <opencv-3.3.1-dev/opencv2/imgproc.hpp>
+// #include <opencv-3.3.1-dev/opencv2/calib3d.hpp>
 #include <opencv-3.3.1-dev/opencv2/opencv.hpp>
 
 using namespace std;
@@ -52,9 +53,28 @@ typedef struct tracker_point{
 enum class FRAME_STATE{
     first,
     second,
+    third,
     more
 };
 #endif
+
+enum class CLUSTER_STATE{
+  missing,    // once no matching
+  tracking,   // tracking more than 2 frames
+  unstable    // first frame tracker
+};
+
+
+
+typedef struct kalman_filter{
+  cv::KalmanFilter kf;      // kalman filter of one cluster
+  Eigen::VectorXd pred_state;   // predicted state
+  int lose_frame;           // record the lost frames
+  int track_frame;          // record the hit frames
+  MOTION_STATE motion;      // the motion of the cluster
+  CLUSTER_STATE cluster_state; // the stability of the cluster
+  int cluster_id;
+}cluster_filter;
 
 typedef struct dbpda_neighbor_info
 {
@@ -84,12 +104,11 @@ class dbpda
 {
     private:
         std::vector< std::vector<cluster_point> > points_history;   // points (t, t-1, t-2), past points(including current scan)
-        std::vector< cluster_point > points_t_current;                // points (t), current points
-        std::vector< dbpda_info > cluster_queue;
-        std::vector< int > cluster_order;                     // record the cluster result
-        std::vector<int> center_list_info;
-        std::vector<cluster_point> center, final_center;
-        pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
+        std::vector< dbpda_info > cluster_queue;                    // the queue of the current scan data info
+        std::vector< int > cluster_order;                           // record the cluster result(contains current and past data)
+        std::vector<cluster_point> final_center;                    // get the cluster center
+        std::vector< std::vector< std::vector<cluster_point> > > history_points_with_cluster_order; // the vector ->  cluster order(time order(data))
+        pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;            // used for kd-tree
         dbpda_para dbpda_param; // the parameter of the DBPDA Algorithm
         int history_frame_num;  // the frame numbers that decide to use
         double vel_scale;
@@ -97,18 +116,33 @@ class dbpda
         dbpda_neighbor_info find_neighbors(cluster_point p);
         void expand_neighbor(std::vector< cluster_point > &process_data, std::vector<int> &temp_cluster, int core_index);
         double distance(cluster_point p1, cluster_point p2);
+        double mahalanobis_distance(Eigen::VectorXd v, cluster_filter cluster_kf);
         std::vector<int> split_past(std::vector< cluster_point > process_data, std::vector< std::vector<int> > &final_cluster_order);
         void cluster_center(std::vector< std::vector<cluster_point> > cluster_list);
-        void kalman_filter_init();
+        
+        int stateDim = 4;   // kalman filter state : [x,y,vx,vy]
+        int measureDim = 4; // [x,y]
+        int controlDim = 0;
+        int lose_max = 3;
+        int cluster_count = 0;  // the counter to calculate the current occupied cluster id
+        std::vector<cluster_filter> cluster_past;       // record the cluster filter result
+        void kalman_filter_init(std::vector<cluster_point> cluster_list, int init_index);
+        void kalman_filter_update_clusterPt(std::vector<cluster_point> cluster_update, cv::KalmanFilter &kf);
+        void kalman_association(std::vector< cluster_point > process_data, std::vector< std::vector<int> > final_cluster_order);
+        std::vector<cluster_point> time_encode();
     public:
         dbpda(double eps=2.5, int Nmin=2);
         ~dbpda();
         std::vector< std::vector<cluster_point> > cluster(std::vector<cluster_point> data);
         std::vector<cluster_point> get_center(void);
         std::vector<cluster_point> get_history_points(void);
+        std::vector< std::vector< std::vector<cluster_point> > > get_history_points_with_cluster_order(void);
+        void set_parameter(double eps, int Nmin, int frames_num);
         int scan_num;
         double data_period;
         bool output_info;       // decide to print the dbscan output information or not
+        bool show_kf_info;
+        bool use_kf;
 
 };
 
--- src/dbpda.cpp
+++ src/dbpda.cpp
@@ -3,10 +3,13 @@
 dbpda::dbpda(double eps, int Nmin){
     dbpda_param.eps = eps;
     dbpda_param.Nmin = Nmin;
-    history_frame_num = 3;
+    history_frame_num = 4;
+    points_history.clear();
     vel_scale = 1;
     input_cloud = pcl::PointCloud<pcl::PointXYZ>().makeShared();
     output_info = true;
+    show_kf_info = true;
+    use_kf = false;
 }
 
 dbpda::~dbpda(){}
@@ -17,6 +20,7 @@ std::vector< std::vector<cluster_point> > dbpda::cluster(std::vector<cluster_poi
     if(points_history.size()>history_frame_num){
         points_history.erase(points_history.begin());
     }
+    std::cout << "points_history size : " << points_history.size() << std::endl;
     switch (points_history.size())
     {
     case 1:
@@ -24,32 +28,63 @@ std::vector< std::vector<cluster_point> > dbpda::cluster(std::vector<cluster_poi
         break;
     case 2:
         frame_state = FRAME_STATE::second;
+        break;
     default:
         frame_state = FRAME_STATE::more;
         break;
     }
     cluster_queue.clear();
     cluster_queue.shrink_to_fit();
+    // ready to process the cluster points
     std::vector< cluster_point > process_data;
-    for(int i=0;i<points_history.size();i++){
-        for(int j=0;j<points_history.at(i).size();j++){
+    if(frame_state == FRAME_STATE::more && use_kf){
+        std::vector<cluster_point> encode_centers = time_encode();
+        for(int i=0;i<encode_centers.size();i++){
             pcl::PointXYZ temp_pt;
-            temp_pt.x = points_history.at(i).at(j).x;
-            temp_pt.y = points_history.at(i).at(j).y;
-            temp_pt.z = points_history.at(i).at(j).vel * vel_scale;
+            temp_pt.x = encode_centers.at(i).x;
+            temp_pt.y = encode_centers.at(i).y;
+            temp_pt.z = encode_centers.at(i).vel * vel_scale;
             input_cloud->points.push_back(temp_pt);
-            process_data.push_back(points_history.at(i).at(j));
+            process_data.push_back(encode_centers.at(i));
+            // initial the reachable_dist
+            dbpda_info cluster_pt;
+            cluster_pt.reachable_dist = -1; // undefined distance
+            cluster_queue.push_back(cluster_pt);
+        }
+        for(int i=0;i<data.size();i++){
+            pcl::PointXYZ temp_pt;
+            temp_pt.x = data.at(i).x;
+            temp_pt.y = data.at(i).y;
+            temp_pt.z = data.at(i).vel * vel_scale;
+            input_cloud->points.push_back(temp_pt);
+            process_data.push_back(data.at(i));
             // initial the reachable_dist
             dbpda_info cluster_pt;
             cluster_pt.reachable_dist = -1; // undefined distance
             cluster_queue.push_back(cluster_pt);
         }
     }
-
+    else{
+        for(int i=0;i<points_history.size();i++){
+            for(int j=0;j<points_history.at(i).size();j++){
+                pcl::PointXYZ temp_pt;
+                temp_pt.x = points_history.at(i).at(j).x;
+                temp_pt.y = points_history.at(i).at(j).y;
+                temp_pt.z = points_history.at(i).at(j).vel * vel_scale;
+                input_cloud->points.push_back(temp_pt);
+                process_data.push_back(points_history.at(i).at(j));
+                // initial the reachable_dist
+                dbpda_info cluster_pt;
+                cluster_pt.reachable_dist = -1; // undefined distance
+                cluster_queue.push_back(cluster_pt);
+            }
+        }
+    }
     cluster_order.clear();
     cluster_order.shrink_to_fit();
     std::vector< std::vector<int> > final_cluster_order;
     
+    // DBSCAN cluster
     for(int i=0;i<process_data.size();i++){
         // check if the pt has been visited
         if(process_data.at(i).vistited)
@@ -73,8 +108,37 @@ std::vector< std::vector<cluster_point> > dbpda::cluster(std::vector<cluster_poi
         }
         final_cluster_order.push_back(temp_cluster);
     }
-    std::vector<int> current_order = split_past(process_data, final_cluster_order);
+    
     std::vector< std::vector<cluster_point> > cluster_result;
+    // init the kalman filter from the third frame
+    if(frame_state == FRAME_STATE::second && use_kf){
+        for(int i=0;i<final_cluster_order.size();i++){
+            std::vector<cluster_point> temp_cluster_unit;
+            for(int j=0;j<final_cluster_order.at(i).size();j++){
+                temp_cluster_unit.push_back(process_data.at(final_cluster_order.at(i).at(j)));
+            }
+            cluster_result.push_back(temp_cluster_unit);
+        }
+        cluster_center(cluster_result);
+        for(int j=0;j<cluster_result.size();j++){
+            kalman_filter_init(cluster_result.at(j),j);
+        }
+    }
+    else if(frame_state == FRAME_STATE::more && use_kf){
+        for(int i=0;i<final_cluster_order.size();i++){
+            std::vector<cluster_point> temp_cluster_unit;
+            for(int j=0;j<final_cluster_order.at(i).size();j++){
+                temp_cluster_unit.push_back(process_data.at(final_cluster_order.at(i).at(j)));
+            }
+            cluster_result.push_back(temp_cluster_unit);
+        }
+        cluster_center(cluster_result);
+        std::cout << "before kalman association\n";
+        kalman_association(process_data,final_cluster_order);
+        std::cout << "after kalman association\n";
+    }
+    cluster_result.clear();
+    std::vector<int> current_order = split_past(process_data, final_cluster_order);
     for(int i=0;i<final_cluster_order.size();i++){
         std::vector<cluster_point> temp_cluster_unit;
         for(int j=0;j<final_cluster_order.at(i).size();j++){
@@ -83,6 +147,7 @@ std::vector< std::vector<cluster_point> > dbpda::cluster(std::vector<cluster_poi
         cluster_result.push_back(temp_cluster_unit);
     }
     cluster_center(cluster_result);
+    
     return cluster_result;
 }
 
@@ -91,6 +156,13 @@ double dbpda::distance(cluster_point p1, cluster_point p2){
     return d.norm();
 }
 
+double mahalanobis_distance(Eigen::VectorXd v, cluster_filter cluster_kf){
+    cv::KalmanFilter kf = cluster_kf.kf;
+    Eigen::MatrixXd S;
+    cv::Mat Cov = kf.measurementMatrix * kf.errorCovPre * kf.measurementMatrix.t() + kf.measurementNoiseCov;
+    cv::cv2eigen(Cov,S);
+    return std::sqrt(v.transpose()*S.inverse()*cluster_kf.pred_state);
+}
 
 dbpda_neighbor_info dbpda::find_neighbors(cluster_point p){
     pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
@@ -153,6 +225,30 @@ std::vector<int> dbpda::split_past(std::vector< cluster_point > process_data, st
             current_order.push_back(cluster_order.at(i));
         }
     }
+    // get the history data with time series
+    std::vector< std::vector< std::vector<cluster_point> > > points_cluster_vec;    // contains the cluster result with all datas(need current data)
+    std::vector< std::vector< std::vector<cluster_point> > > past_points_cluster_vec;   // the cluster result that only contain past data
+    for(int i=0;i<final_cluster_order.size();i++){
+        std::vector< std::vector<cluster_point> > temp_past_data(history_frame_num);
+        int past_scan_id_count = 0;
+        for(int j=0;j<final_cluster_order.at(i).size();j++){
+            int past_timestamp = process_data.at(final_cluster_order.at(i).at(j)).scan_id;
+            if(past_timestamp != scan_num){
+                past_scan_id_count++;
+            }
+            temp_past_data.at(history_frame_num-1-(scan_num-past_timestamp)).push_back(process_data.at(final_cluster_order.at(i).at(j)));
+        }
+        if(past_scan_id_count != final_cluster_order.at(i).size()){
+            points_cluster_vec.push_back(temp_past_data);
+        }
+        else{
+            past_points_cluster_vec.push_back(temp_past_data);
+        }
+    }
+    points_cluster_vec.insert(points_cluster_vec.end(),past_points_cluster_vec.begin(),past_points_cluster_vec.end());
+    history_points_with_cluster_order.clear();
+    history_points_with_cluster_order = points_cluster_vec;
+
     // remove the cluster that only contains past data
     for(std::vector< std::vector<int> >::iterator it=final_cluster_order.begin();it != final_cluster_order.end();){
         int count = 0;
@@ -183,7 +279,7 @@ std::vector<int> dbpda::split_past(std::vector< cluster_point > process_data, st
 }
 
 void dbpda::cluster_center(std::vector< std::vector<cluster_point> > cluster_list){
-    center_list_info.assign(cluster_list.size(),0);
+    std::vector<cluster_point> center;
     center.clear();
     final_center.clear();
     for(int cluster_idx=0;cluster_idx<cluster_list.size();cluster_idx++){
@@ -202,7 +298,6 @@ void dbpda::cluster_center(std::vector< std::vector<cluster_point> > cluster_lis
             pt.y_v += cluster_list.at(cluster_idx).at(i).y_v;
             pt.z_v += cluster_list.at(cluster_idx).at(i).z_v;
         }
-        center_list_info.at(cluster_idx) = cluster_list.at(cluster_idx).size();
         pt.x /= cluster_list.at(cluster_idx).size();
         pt.y /= cluster_list.at(cluster_idx).size();
         pt.z /= cluster_list.at(cluster_idx).size();
@@ -231,4 +326,169 @@ std::vector<cluster_point> dbpda::get_history_points(void){
         }
     }
     return multi_frame_pts;
+}
+
+std::vector< std::vector< std::vector<cluster_point> > > dbpda::get_history_points_with_cluster_order(void){
+    return history_points_with_cluster_order;
+}
+
+void dbpda::set_parameter(double eps, int Nmin, int frames_num){
+    dbpda_param.eps = eps;
+    dbpda_param.Nmin = Nmin;
+    history_frame_num = frames_num;
+}
+
+void dbpda::kalman_filter_init(std::vector<cluster_point> cluster_list, int init_index){
+    cluster_filter cluster_kf;
+    cv::KalmanFilter kf;
+    kf.init(stateDim, measureDim,controlDim,CV_64F);
+    // std::cout << "Init kf" << std::endl;
+    kf.transitionMatrix = (cv::Mat_<double>(4,4) << 1,0,data_period,0,
+                                                    0,1,0,data_period,
+                                                    0,0,1,0,
+                                                    0,0,0,1);
+    cv::setIdentity(kf.measurementMatrix);
+    cv::setIdentity(kf.processNoiseCov, cv::Scalar(0.01));
+    // cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(0.099,0.002,0.028,0.028));
+    cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(0.1));
+    cv::Mat new_state;
+    kf.statePost.at<double>(0) = final_center.at(init_index).x;
+    kf.statePost.at<double>(1) = final_center.at(init_index).y;
+    kf.statePost.at<double>(2) = final_center.at(init_index).x_v;
+    kf.statePost.at<double>(3) = final_center.at(init_index).y_v;
+
+    // update the cluster points to one cluster
+    kalman_filter_update_clusterPt(cluster_list,kf);
+    // std::cout << "Finish update kf" << std::endl;
+    cv::Mat pred = kf.predict();
+    Eigen::VectorXd init_pred(4);
+    init_pred << pred.at<double>(0),
+                 pred.at<double>(1),
+                 pred.at<double>(2),
+                 pred.at<double>(3);
+    cluster_kf.kf = kf;
+    cluster_kf.pred_state = init_pred;
+    cluster_kf.lose_frame = cluster_kf.track_frame = 0;
+    if(final_center.at(init_index).vel < 0.1)
+        cluster_kf.motion = MOTION_STATE::stop;
+    else
+        cluster_kf.motion = MOTION_STATE::move;
+    cluster_kf.cluster_state = CLUSTER_STATE::unstable;
+    cluster_kf.cluster_id = cluster_count++;
+    cluster_past.push_back(cluster_kf);
+}
+
+void dbpda::kalman_filter_update_clusterPt(std::vector<cluster_point> cluster_update, cv::KalmanFilter &kf){
+    // std::cout << "In update kf func" << std::endl;
+    for(int i=0;i<cluster_update.size();i++){
+        cluster_point temp_cluster = cluster_update.at(i);
+        double measurement[4] = {temp_cluster.x,temp_cluster.y,temp_cluster.x_v,temp_cluster.y_v};
+        cv::Mat measureMat = cv::Mat(4,1,CV_64F,measurement);
+        // double measurement[2] = {temp_cluster.x,temp_cluster.y};
+        // cv::Mat measureMat = cv::Mat(2,1,CV_64F,measurement);
+        cv::Mat estimateMat = kf.correct(measureMat);
+    }
+}
+
+void dbpda::kalman_association(std::vector< cluster_point > process_data, std::vector< std::vector<int> > final_cluster_order){
+    std::cout << "In association kf func\nCluster kf filter size: " << cluster_past.size() << std::endl;
+    std::vector<int> hit_track(cluster_past.size(),0);
+    for(int i=0;i<final_cluster_order.size();i++){
+        int past_kalman_id = -1;
+        std::vector<cluster_point> temp_list;
+        // cluster_point past_kf;
+        for(int j=0;j<final_cluster_order.at(i).size();j++){
+            if(process_data.at(final_cluster_order.at(i).at(j)).cluster_flag != -1){
+                past_kalman_id = process_data.at(final_cluster_order.at(i).at(j)).cluster_flag;
+                // past_kf = process_data.at(final_cluster_order.at(i).at(j));
+            }
+            else{
+                temp_list.push_back(process_data.at(final_cluster_order.at(i).at(j)));
+            }
+        }
+        if(past_kalman_id != -1){
+            int update_order;
+            for(int k=0;k<cluster_past.size();k++){
+                if(past_kalman_id == cluster_past.at(k).cluster_id){
+                    hit_track.at(k)++;
+                    update_order = k;
+                    std::cout << "update kf with cluster_id:" << past_kalman_id << ", filter order: " << k << std::endl;
+                }
+            }
+            // hit_track.at(past_kalman_id)++;
+            kalman_filter_update_clusterPt(temp_list,cluster_past.at(update_order).kf);
+            double current_v = Eigen::Vector2d(cluster_past.at(update_order).pred_state(2),
+                                            cluster_past.at(update_order).pred_state(3)).norm();
+            // update the kalman filter
+            cv::Mat pred = cluster_past.at(update_order).kf.predict();
+            Eigen::VectorXd kf_pred(4);
+            kf_pred << pred.at<double>(0),
+                       pred.at<double>(1),
+                       pred.at<double>(2),
+                       pred.at<double>(3);
+            cluster_past.at(update_order).pred_state = kf_pred;
+            double pred_v = Eigen::Vector2d(kf_pred(2),kf_pred(3)).norm();
+            cluster_past.at(update_order).track_frame++;
+            if(current_v > pred_v){
+                if(cluster_past.at(update_order).motion == MOTION_STATE::move || cluster_past.at(update_order).motion == MOTION_STATE::slow_down){
+                    cluster_past.at(update_order).motion = MOTION_STATE::slow_down;
+                }
+            }
+            else if(pred_v < 0.1){
+                cluster_past.at(update_order).motion = MOTION_STATE::stop;
+            }
+            else{
+                cluster_past.at(update_order).motion = MOTION_STATE::move;
+            }
+            // std::cout << "Done the update\n";
+        }
+        // initialize the new kalman filter
+        else{
+            kalman_filter_init(temp_list,i);
+        }
+        
+    }
+    for(int i=0;i<hit_track.size();i++){
+        if(hit_track.at(i)==0){
+            cluster_past.at(i).lose_frame++;
+        }
+        else{
+            cluster_past.at(i).lose_frame = 0;
+        }
+    }
+    // delete the lose kalman filters
+    for(std::vector< cluster_filter >::iterator it=cluster_past.begin();it != cluster_past.end();){
+        if((*it).lose_frame >= lose_max){
+            // std::cout << "delete kf\n";
+            it = cluster_past.erase(it);
+        }
+        else{
+            it++;
+        }
+    }
+
+}
+
+std::vector<cluster_point> dbpda::time_encode(){
+    std::cout << "In time encode func" << std::endl;
+    std::vector<cluster_point> encode_centers;
+    for(int i=0;i<cluster_past.size();i++){
+        cluster_point temp;
+        temp.x = cluster_past.at(i).pred_state(0);
+        temp.y = cluster_past.at(i).pred_state(1);
+        temp.x_v = cluster_past.at(i).pred_state(2);
+        temp.y_v = cluster_past.at(i).pred_state(3);
+        temp.vel = Eigen::Vector2d(temp.x_v,temp.y_v).norm();
+        temp.vistited = false;
+        temp.scan_id = scan_num-1;
+        temp.cluster_flag = cluster_past.at(i).cluster_id;  // assign the past cluster id
+        encode_centers.push_back(temp);
+        if(show_kf_info){
+            std::cout << "============================================\n";
+            std::cout << "Cluster KF @" << i << " with id:" << temp.cluster_flag << std::endl;
+            std::cout << "Pos: (" << temp.x << ", " << temp.y
+                      << "), Vel: " << temp.vel << "(" << temp.x_v << ", " << temp.y_v << ")\n";
+        }
+    }
+    return encode_centers;
 }
