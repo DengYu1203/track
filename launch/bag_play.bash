874b0fde41cc131af856e393bfd1d1797d868df0
DengYu1203 <sg110333@gmail.com>
2021-01-19 16:34:03 +0800

    add new bag play path

--- launch/bag_play.bash
+++ launch/bag_play.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 rate=0.5
 bag_path_1=/data2/nuScenes/bag_sweep_final/log46_1537295833898809_scene-0626.bag
-bag_path_2=/data2/nuScenes/bag_sweep_final/log3*.bag
-for f in $bag_path_1
+bag_path_2=/data2/nuScenes/bag_sweep_final/log0_*.bag
+bag_path_3=/data2/nuScenes/bag_sweep_final/log46_1537295813898777_scene-0625.bag
+for f in $bag_path_2
 do
   rosbag play $f --clock -r $rate
 done
+
+# rosbag play $bag_path_3 $bag_path_1 --clock -r $rate
