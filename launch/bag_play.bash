8e58eb86f6ceea97a540627c0320c23ad8583888
DengYu1203 <sg110333@gmail.com>
2021-04-23 15:16:31 +0800

    add bag folder

--- launch/bag_play.bash
+++ launch/bag_play.bash
@@ -3,7 +3,8 @@ rate=0.5
 bag_path_1=/data2/nuScenes/bag_sweep_final/log46_1537295833898809_scene-0626.bag
 bag_path_2=/data2/nuScenes/bag_sweep_final/log0_*.bag
 bag_path_3=/data2/nuScenes/bag_sweep_final/log46_1537295813898777_scene-0625.bag
-for f in $bag_path_2
+bag_path=($bag_path_3 $bag_path_1)
+for f in $bag_path_1
 do
   rosbag play $f --clock -r $rate
 done
